7084fd01e5dbe1546cd019a726b7d09fc90341b7
yishuenlo <lo.yishuen@gmail.com>
2020-07-10 17:09:08 -0600

    refactor code

--- js/CodeWars/6kyu/eqSidesOfArr.js
+++ js/CodeWars/6kyu/eqSidesOfArr.js
@@ -11,17 +11,11 @@
 
 let arr = [1, 2, 3, 4, 3, 2, 1];
 
-function balance(arr, index) {
-  let left = arr.slice(0, index).reduce((a, b) => a + b, 0);
-  let right = arr.slice(index + 1).reduce((a, b) => a + b, 0);
-  return left === right ? index : -1;
-}
-
 function findEvenIndex(arr) {
+  //loop through array
   let index = 0;
 
   do {
-    //find sum of left and right based on index
     let left = arr.slice(0, index).reduce((a, b) => a + b, 0);
     let right = arr.slice(index + 1).reduce((a, b) => a + b, 0);
 
@@ -29,6 +23,22 @@ function findEvenIndex(arr) {
     if (left === right) return index;
     index++;
   } while (index < arr.length);
+
+  //if no match, return -1
+  return -1;
+}
+
+//refactored, faster code
+function findEvenIndex(arr) {
+  //loop through array, through index one and second to last index
+  for (let i = 1; i < arr.length - 1; i++) {
+    //find sum of left and right based on index
+    let left = arr.slice(0, i).reduce((a, b) => a + b, 0);
+    let right = arr.slice(i + 1).reduce((a, b) => a + b, 0);
+
+    //return index if left equals right
+    if (left === right) return i;
+  }
 
   //if no match, return -1
   return -1;
