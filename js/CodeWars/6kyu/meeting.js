67aab7e1c34571ae0485fa8b34d66e379a153524
yishuenlo <lo.yishuen@gmail.com>
2020-07-10 14:10:03 -0600

    add comments

--- js/CodeWars/6kyu/meeting.js
+++ js/CodeWars/6kyu/meeting.js
@@ -3,22 +3,17 @@
 //convert to string, Fred:Corwill = (Corwill, Fred)
 //sort alphabetically
 
+//input = string
+//output = string
+
 const s =
   "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
 
-let names = s.toUpperCase().split(';');
-
-let regex = /([A-Z]+):([A-Z]+)/gi;
-
-names = names.map(name => name.replace(regex, '($2, $1)')).sort().join("");
-
-console.log('names', names)
-
 function meeting(s) {
   return s
-    .toUpperCase()
-    .split(";")
-    .map((name) => name.replace(/([A-Z]+):([A-Z]+)/gi, "($2, $1)"))
-    .sort()
-    .join("");
+    .toUpperCase() //convert to uppercase
+    .split(";") //conver to array
+    .map((name) => name.replace(/([A-Z]+):([A-Z]+)/gi, "($2, $1)")) //swap first and last names
+    .sort() //sort
+    .join(""); //convert to string
 }
