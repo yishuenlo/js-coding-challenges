c059cc075a1078cdc85bdc6a9eb001bf6dbc779f
Yishuen Lo <lo.yishuen@gmail.com>
2020-07-21 18:32:03 -0600

    log

---
+++ js/leetcode/Easy/bestTimeToStock.js
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+//an array of stock price
+//can only execute once per day
+//find maximum profit
+
+const stock = [7, 1, 5, 3, 6, 4];
+
